b43bd47429d2b0caefcea05215d75124324a6bc5
TypeA2 <nuanlim29@gmail.com>
2019-01-15 23:32:14 +0100

    Basic setup of NaoQt and libNaoQt

---
+++ NaoQt/src/NaoQt.cpp
@@ -0,0 +1,24 @@
+/*
+    This file is part of NaoQt.
+
+    NaoQt is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NaoQt is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with NaoQt.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#include "NaoQt.h"
+
+NaoQt::NaoQt(QWidget *parent)
+    : QMainWindow(parent) {
+
+}
