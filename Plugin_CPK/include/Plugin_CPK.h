3b9625f8385cabca4aabd952a42355fdf41fd08c
TypeA2 <nuanlim29@gmail.com>
2019-02-24 01:42:22 +0100

    Plugin_CPK skeleton framework

---
+++ Plugin_CPK/include/Plugin_CPK.h
@@ -0,0 +1,74 @@
+/*
+    This file is part of libnao.
+
+    libnao is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libnao is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with libnao.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <libnao.h>
+
+#include <Plugin/NaoPlugin.h>
+
+LIBNAO_PLUGIN_CALL LIBNAO_PLUGIN_DECL NaoPlugin GetNaoPlugin();
+
+namespace Plugin {
+    namespace PluginInfo {
+        LIBNAO_PLUGIN_DECL NaoString name();
+        LIBNAO_PLUGIN_DECL NaoString display_name();
+        LIBNAO_PLUGIN_DECL NaoString description();
+        LIBNAO_PLUGIN_DECL uint64_t version();
+
+    }
+
+    namespace AuthorInfo {
+        LIBNAO_PLUGIN_DECL NaoString name();
+        LIBNAO_PLUGIN_DECL NaoString text_plain();
+        LIBNAO_PLUGIN_DECL NaoString text_rich();
+    }
+
+    namespace Error {
+        LIBNAO_PLUGIN_DECL const NaoString& get_error();
+
+        NaoString& error();
+    }
+
+    namespace Description {
+        LIBNAO_PLUGIN_DECL bool prioritise_description();
+        LIBNAO_PLUGIN_DECL NaoString description();
+    }
+
+    namespace Capabilities {
+        LIBNAO_PLUGIN_DECL bool supports(NaoObject* object);
+        LIBNAO_PLUGIN_DECL bool populatable(NaoObject* object);
+        LIBNAO_PLUGIN_DECL bool decodable(NaoObject* object);
+        LIBNAO_PLUGIN_DECL bool can_move(NaoObject* from, NaoObject* to);
+    }
+
+    namespace Function {
+        LIBNAO_PLUGIN_DECL bool populate(NaoObject* object);
+        LIBNAO_PLUGIN_DECL bool decode(NaoObject* object, NaoIO* out);
+        LIBNAO_PLUGIN_DECL bool move(NaoObject*& from, NaoObject* to);
+    }
+
+    namespace ContextMenu {
+        LIBNAO_PLUGIN_DECL bool has_context_menu(NaoObject* object);
+        LIBNAO_PLUGIN_DECL NaoPlugin::ContextMenu::type context_menu(NaoObject* object);
+    }
+
+    namespace Extraction {
+        LIBNAO_PLUGIN_DECL bool extract_single_file(NaoObject* object);
+        LIBNAO_PLUGIN_DECL bool extract_all_files(NaoObject* object);
+    }
+}
---
+++ Plugin_CPK/src/Plugin_CPK.cpp
@@ -0,0 +1,182 @@
+/*
+    This file is part of libnao.
+
+    libnao is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libnao is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with libnao.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "Plugin_CPK.h"
+
+#define N_LOG_ID "Plugin_DAT"
+#include <Logging/NaoLogging.h>
+
+NaoPlugin GetNaoPlugin() {
+    using namespace Plugin;
+    return NaoPlugin {
+        GetNaoPlugin,
+        Error::get_error,
+
+        NaoPlugin::PluginInfo {
+            PluginInfo::name,
+            PluginInfo::display_name,
+            PluginInfo::description,
+            PluginInfo::version
+            },
+
+        NaoPlugin::AuthorInfo {
+            AuthorInfo::name,
+            AuthorInfo::text_plain,
+            AuthorInfo::text_rich
+            },
+
+        NaoPlugin::Description {
+            Description::prioritise_description,
+            Description::description
+            },
+
+        NaoPlugin::Capabilities {
+            Capabilities::supports,
+            Capabilities::populatable,
+            Capabilities::decodable,
+            Capabilities::can_move
+            },
+
+        NaoPlugin::Functionality {
+            Function::populate,
+            Function::decode,
+            Function::move
+            },
+
+        NaoPlugin::ContextMenu {
+            ContextMenu::has_context_menu,
+            ContextMenu::context_menu
+            }
+    };
+}
+
+namespace Plugin {
+    namespace PluginInfo {
+        NaoString name() {
+            return "libnao CPK plugin";
+        }
+
+        NaoString display_name() {
+            return "libnao CPK";
+        }
+
+        NaoString description() {
+            return "Adds support for CPK archives";
+        }
+
+        uint64_t version() {
+            return 1;
+        }
+
+    }
+
+    namespace AuthorInfo {
+        NaoString name() {
+            return "TypeA2/I_Copy_Jokes";
+        }
+
+        NaoString text_plain() {
+            return "License: LGPLv3 or later\n"
+                "Github: https://github.com/TypeA2\n"
+                "Steam: https://steamcommunity.com/id/TypeA2/";
+
+        }
+
+        NaoString text_rich() {
+            return "License: LGPLv3 or later<br>"
+                "<a href=\"https://github.com/TypeA2\">Github</a><br>"
+                "<a href=\"https://steamcommunity.com/id/TypeA2/\">Steam</a>";
+        }
+
+    }
+
+    namespace Error {
+        const NaoString& get_error() {
+            return error();
+        }
+
+        NaoString& error() {
+            static NaoString err;
+
+            return err;
+        }
+    }
+
+    namespace Description {
+        bool prioritise_description() {
+            return true;
+        }
+
+        NaoString description() {
+            return "CPK Archive";
+        }
+    }
+
+    namespace Capabilities {
+        bool supports(NaoObject* object) {
+            return populatable(object);
+        }
+
+        bool populatable(N_UNUSED NaoObject* object) {
+            return false;
+        }
+
+        bool decodable(N_UNUSED NaoObject* object) {
+            return false;
+        }
+
+        bool can_move(N_UNUSED NaoObject* from, N_UNUSED NaoObject* to) {
+            return false;
+        }
+    }
+
+    namespace Function {
+        bool populate(N_UNUSED NaoObject* object) {
+
+            return false;
+        }
+
+        bool decode(N_UNUSED NaoObject* object, N_UNUSED NaoIO* out) {
+            return false;
+        }
+
+        bool move(N_UNUSED NaoObject*& from, N_UNUSED NaoObject* to) {
+            return false;
+        }
+    }
+
+    namespace ContextMenu {
+        bool has_context_menu(N_UNUSED NaoObject* object) {
+            return false;
+        }
+
+        NaoPlugin::ContextMenu::type context_menu(N_UNUSED NaoObject* object) {
+            return NaoPlugin::ContextMenu::type();
+        }
+    }
+
+    namespace Extraction {
+        bool extract_single_file(N_UNUSED NaoObject* object) {
+            return false;
+        }
+
+        bool extract_all_files(N_UNUSED NaoObject* object) {
+            return false;
+        }
+    }
+}
+
