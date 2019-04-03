32174fb423200f287ca5191c21e63a191393b63d
TypeA2 <nuanlim29@gmail.com>
2019-04-03 15:35:50 +0200

    Update NaoPlugin to an abstract class

--- libnao/include/Plugin/NaoPlugin.h
+++ libnao/include/Plugin/NaoPlugin.h
@@ -28,51 +28,54 @@
 class NaoObject;
 class NaoIO;
 
-struct LIBNAO_API NaoPlugin;
+class LIBNAO_API NaoPlugin;
 
-using PluginFunc = NaoPlugin(*)();
+using PluginFunc = NaoPlugin*(*)();
 
-struct LIBNAO_API NaoPlugin {
-    PluginFunc plugin;
+class LIBNAO_API NaoPlugin {
+    public:
+    virtual ~NaoPlugin();
 
-    const NaoString&(*error)();
+    // Plugin info
+    N_NODISCARD virtual NaoString Name() const = 0;
+    N_NODISCARD virtual NaoString DisplayName() const = 0;
+    N_NODISCARD virtual NaoString PluginDescription() const = 0;
+    N_NODISCARD virtual NaoString VersionString() const = 0;
 
-    struct PluginInfo {
-        NaoString(*name)();
-        NaoString(*display_name)();
-        NaoString(*desc)();
-        uint64_t(*version)();
-    } plugin_info;
-    
-    struct AuthorInfo {
-        NaoString(*name)();
-        NaoString(*text_plain)();
-        NaoString(*text_rich)();
-    } author_info;
+    // Author info
+    N_NODISCARD virtual NaoString AuthorName() const = 0;
+    N_NODISCARD virtual NaoString AuthorDescription() const = 0;
 
-    struct Description {
-        bool(*prioritise)();
-        NaoString(*get)();
-    } description;
+    // Supported file description
+    N_NODISCARD virtual bool Prioritise() const = 0;
+    N_NODISCARD virtual NaoString Description() const = 0;
+    N_NODISCARD virtual NaoString Description(NaoObject* of) const = 0;
 
-    struct Capabilities {
-        bool(*supports)(NaoObject* object);
-        bool(*populatable)(NaoObject* object);
-        bool(*decodable)(NaoObject* object);
-        bool(*can_move)(NaoObject* from, NaoObject* to);
-    } capabilities;
+    // Entering
+    N_NODISCARD virtual bool CanEnter(NaoObject* object) = 0;
+    virtual bool Enter(NaoObject* object) = 0;
 
-    struct Functionality {
-        bool(*populate)(NaoObject* object);
-        bool(*decode)(NaoObject* object, NaoIO* out);
-        bool(*move)(NaoObject*& from, NaoObject* to);
-    } functionality;
-    
-    struct ContextMenu {
-        using Entry = NaoPair<NaoString, bool(*)(NaoObject* object)>;
-        using type = NaoVector<Entry>;
-        
-        bool(*has_context_menu)(NaoObject* object);
-        type(*get)(NaoObject* object);
-    } context_menu;
+    // Leaving
+    N_NODISCARD virtual bool ShouldLeave(NaoObject* object) = 0;
+    virtual bool Leave(NaoObject* object) = 0;
+
+    // Moving
+    N_NODISCARD virtual bool CanMove(NaoObject* object) = 0;
+    virtual bool Move(NaoObject* object) = 0;
+
+    // Decoding
+    N_NODISCARD virtual bool CanDecode(NaoObject* object) = 0;
+    virtual bool Decode(NaoObject* object, NaoIO* output) = 0;
+
+    // Context menu
+    class NaoContextMenuEntry {
+        public:
+        virtual ~NaoContextMenuEntry() = 0;
+
+        N_NODISCARD virtual NaoString EntryName() = 0;
+        virtual bool Execute(NaoObject* object) = 0;
+    };
+
+    N_NODISCARD virtual bool HasContextMenu(NaoObject* object) = 0;
+    N_NODISCARD virtual NaoVector<NaoContextMenuEntry> ContextMenu(NaoObject* object) = 0;
 };
